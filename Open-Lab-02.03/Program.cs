cfaf26a606d0a7097f567d28e9894451e69e9a39
matejslovak <joggurtik1@gmail.com>
2019-10-28 08:28:01 +0100

    gf

---
+++ Open-Lab-02.03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Open_Lab_02._03
+{
+    class Program
+    {
+        public static int Sum(int a, int b)
+        {
+            return (a % b);
+        }
+        static void Main(string[] args)
+        {
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Sum(n1, n2));
+        }
+    }
+}
